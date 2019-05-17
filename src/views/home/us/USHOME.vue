42e49db4f434befbdc8ee51666e5fbbf08866b9a
chen <1850322109@qq.com>
2019-05-17 17:31:24 +0800

    css

--- src/views/home/us/USHOME.vue
+++ src/views/home/us/USHOME.vue
@@ -1357,7 +1357,7 @@
                       <img class="u-clients" src="../../../assets/images/fenwick_logo.png" alt="Image Description">
                     </div>
                      <div class="js-slide">
-                      <img class="u-clients" style="height:31px;" src="../../../assets/images/fenwick_logo1.png" alt="Image Description">
+                      <img class="u-clients"  src="../../../assets/images/gco_logo.png" alt="Image Description">
                     </div>
                     <div class="js-slide">
                       <img class="u-clients" src="assets/svg/logos/parnter/partner-logo3.png" alt="Image Description">
