9c72a318756f78631c350e70c860ea4c310ac9bf
liuliang2 <liuliang2@55.com>
2019-06-10 16:37:26 +0800

    add partner

--- src/views/home/us/USHOME.vue
+++ src/views/home/us/USHOME.vue
@@ -1315,6 +1315,9 @@
                      <div class="js-slide">
                       <img class="u-clients"  src="../../../assets/images/gco_logo.png" alt="Image Description">
                     </div>
+                     <div class="js-slide">
+                      <img class="u-clients"  src="../../../assets/images/parnter5.png" alt="Image Description">
+                    </div>
                     <div class="js-slide">
                       <img class="u-clients" src="assets/svg/logos/parnter/partner-logo3.png" alt="Image Description">
                     </div>
