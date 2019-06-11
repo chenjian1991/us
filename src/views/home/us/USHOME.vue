24ae4b5a50dfa9c014169f70080119cfeb5475bb
chen <1850322109@qq.com>
2019-06-11 09:20:25 +0800

    logo no visa

--- src/views/home/us/USHOME.vue
+++ src/views/home/us/USHOME.vue
@@ -782,7 +782,7 @@
                         <div class="col-5 px-2 mb-3">
                           <!-- Fancybox -->
                           <a>
-                            <img class="img-fluid rounded" src="../../../assets/images/home/usHome/bottomright.jpg" alt="Image Description">
+                            <img class="img-fluid rounded" src="../../../assets/images/home/usHome/new_no_visa.png" alt="Image Description">
 
                           </a >
                           <!-- End Fancybox -->
