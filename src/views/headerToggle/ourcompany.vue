d922382cc28504901949841b525d47aff508f24c
tianqin <1492585850@qq.com>
2019-08-08 12:36:00 +0800

    kong

--- src/views/headerToggle/ourcompany.vue
+++ src/views/headerToggle/ourcompany.vue
@@ -17,7 +17,7 @@
       <div class="row" >
         <div class="col-lg-12 space-lg-2">
           <div>
-             <p>Based in the US,  55 TRADE supports the major crypto currencies: Bitcoin, Ether and Litecoin, against each other or the US Dollar, or our own stable coin: USDD (US Digital). 55 TRADE offers its users, a range of cryptocurrencies to buy/sell and store, on our secure platform. Users have secured wallets available to them on the 55 TRADE platform, and deposits or withdrawals from and to wallets is simple. Users can fund their accounts with either crypto or fiat, and we support USD deposits and withdrawals.  </p>
+             <p>Based in the US, 55 TRADE supports the major crypto currencies: Bitcoin, Ether and Litecoin, against each other or the US Dollar, or our own stable coin: USDD (US Digital). 55 TRADE offers its users, a range of cryptocurrencies to buy/sell and store, on our secure platform. Users have secured wallets available to them on the 55 TRADE platform, and deposits or withdrawals from and to wallets is simple. Users can fund their accounts with either crypto or fiat, and we support USD deposits and withdrawals.  </p>
 
              <h2 class="h4">Our Compliance</h2> 
              <p> 55 TRADE is officially registered as a Money Service Business (MSB) with U.S. regulators, administered by the Financial Crimes Enforcement Network (FinCEN).</p>
@@ -135,8 +135,8 @@
 </script>
 <style lang="less">
 .text-primary{
-    color:#12869A !important;
-}
+      color:#12869A !important;
+  }
 a.text-primary:hover, a.text-primary:focus{
   color:#12869A !important;
 }
@@ -153,7 +153,7 @@ a.text-primary:hover, a.text-primary:focus{
         font-size: 2.5rem;
     }
     #bg-img{
-        background: url('../../assets/images/headertoggle/common_hero.jpeg') no-repeat center;
+        background: url('../../assets/images/headertoggle/common_hero.jpeg') no-repeat;
         height: 120%;
         background-size: cover;
     }
