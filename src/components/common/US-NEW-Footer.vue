cf9be7ebadd0f53d729cdd5fa1bf758f2f8c11a3
tianqin <1492585850@qq.com>
2019-08-12 14:19:49 +0800

    delete

--- src/components/common/US-NEW-Footer.vue
+++ src/components/common/US-NEW-Footer.vue
@@ -139,7 +139,7 @@
                 <img src="assets/svg/logos/logo-white-footer.svg" alt="Logo" style="width: 50px;">
                 </a>
                 <!-- target="_blank" href="https://www.55.com/MSB.pdf" -->
-                <p class="small text-white"><a style="color:#fff;" >MSB number is: 31000151244818</a> </p>
+                <p class="small text-white"><a style="color:#fff;" >MSB No. 31000151244818</a> </p>
                 <p class="small text-white">2500 Plaza 5, Unit 2553, Jersey City, NJ 07311</p>
                 <p class="small text-white">&copy; 2017-2019 55.trade All rights reserved.</p>
                 <div class="col-lg-6 text-lg-right" style="max-width:100%">
