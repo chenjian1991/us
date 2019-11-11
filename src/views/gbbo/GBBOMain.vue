46ae119652706e3bac338d2bcbe79db25a294d86
dongzhou <xf_30el@126.com>
2019-11-11 16:41:38 +0800

    feat: 整理组件

--- src/views/gbbo/GBBOMain.vue
+++ src/views/gbbo/GBBOMain.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="gbbomain">
-    
+    <!-- one 包括盘口  K线 -->
+    <div class="gbbomain-realtime">
+
+    </div>
+    <!-- two 买入卖出 历史成交 -->
+    <div class="gbbomain-transaction">
+
+    </div>
+    <!-- 当前订单，历史订单 -->
+    <div class="gbbomain-order">
+
+    </div>
   </div>
 </template>
 <script>
@@ -8,12 +19,38 @@ export default {
   name: 'gbbo',
   data(){
     return{}
-  }
+  },
+  created() {
+    
+  },
+  mounted() {
+    
+  },
+  props: {
 
+  },
+  computed: {
+    
+  },
+  methods: {
+
+  },
+  components:{
+
+  }
 }
 </script>
 <style lang="less">
 .gbbomain{
-
+  min-width: 1280px;
+  &-realtime{
+    display: flex;
+  }
+  &-transaction{
+    display: flex;
+  }
+  &-order{
+    display: flex;
+  }
 }
 </style>
