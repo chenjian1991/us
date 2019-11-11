c446a7056b2b6b600686e09a87ea3a0f66263d66
dongzhou <xf_30el@126.com>
2019-11-11 16:58:31 +0800

    fix: 划分需求区域

--- src/views/gbbo/GBBOMain.vue
+++ src/views/gbbo/GBBOMain.vue
@@ -2,7 +2,17 @@
   <div class="gbbomain">
     <!-- one 包括盘口  K线 -->
     <div class="gbbomain-realtime">
+      <!-- 盘口 -->
+      <div class="gbbomain-realtime__item">
+        <!-- 切换 header -->
+        <div class="gbbomain-realtime__hd"></div>
+        <!-- 盘口 -->
+        <div class="gbbomain-realtime__box"></div>
+      </div>
+      <!--K线-->
+      <div class="gbbomain-realtime__line">
 
+      </div>
     </div>
     <!-- two 买入卖出 历史成交 -->
     <div class="gbbomain-transaction">
