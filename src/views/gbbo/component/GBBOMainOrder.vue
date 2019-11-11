d8a767f01bdc241eed95256e91536ac1dbb025ec
xueruipan <cheriepan@163.com>
2019-11-11 18:06:30 +0800

    add vue

---
+++ src/views/gbbo/component/GBBOMainOrder.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="gbbomain-order-box">
+    
+  </div>
+</template>
+<script>
+export default {
+  name: "gbbo",
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  props: {},
+  computed: {},
+  methods: {},
+  components: {}
+};
+</script>
+<style lang="less">
+  
+</style>
