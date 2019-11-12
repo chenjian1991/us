61a3f467c6a28bf91b043ec3d5415117a2f64a19
dongzhou <xf_30el@126.com>
2019-11-12 11:09:29 +0800

    feat: 增加k线组件

---
+++ src/views/gbbo/component/GBBOKLine.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="gbboline">
+    
+  </div>
+</template>
+<script>
+export default {
+  name: 'K Line',
+  data() {
+    return {}
+  },
+  created(){
+
+  },
+  mounted(){
+
+  },
+  
+}
+</script>
+<style lang="less">
+  .gbboline{
+    
+  }
+</style>
