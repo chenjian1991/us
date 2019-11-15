cbf76ffb934d64e43563702eec34d2b0ee5a0d3d
apple <weijiadang@xinmoney.cn>
2019-11-15 10:51:52 +0800

    gai

--- src/views/about/about_new.vue
+++ src/views/about/about_new.vue
@@ -2,11 +2,11 @@
   <div id="about">
     <div class="mainTitle">
       <div class="ask container">Who are we?</div>
-      <div class="answer container">We believe that trading should be transparent, neutral, and give everyone the best chance to win. </div>
+      <div class="answer container">We believe that trading should be transparent, neutral, and give everyone the best chance to win.We’re an NYC-based team of visionaries, tech-geeks, enthusiasts, and innovators with a common goal: discover simple solutions to complex problems. Tresso is dedicated to helping our traders realize the best possible outcomes through amplified trading options, speed and efficiency, We’re elevating the trading experience in crypto, token and digital asset markets.  Together, we’re driving trading innovation. </div>
     </div>
-    <div class="describe">
+    <!-- <div class="describe">
       <div class="describeInner container">We’re an NYC-based team of visionaries, tech-geeks, enthusiasts, and innovators with a common goal: discover simple solutions to complex problems. Tresso is dedicated to helping our traders realize the best possible outcomes through amplified trading options, speed and efficiency, We’re elevating the trading experience in crypto, token and digital asset markets.  Together, we’re driving trading innovation.</div>
-    </div>
+    </div> -->
     <div class="team container">
       <div class="title">Meet the Tresso Team</div>
       <div class="members">
