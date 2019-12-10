1d304deda09236da4432ba60fffec0f3763e5d04
chen <1850322109@qq.com>
2019-12-10 14:33:18 +0800

    css

--- api/usersystem.js
+++ api/usersystem.js
@@ -4,6 +4,35 @@ import {userStatus} from './urls'
 export const queryUserStatus = (params)=>{
     return getApi(userStatus, params);
 }
+export const onloadCallback = (id,successCallback,expiredCallback,errorCallback) =>{
+                if(grecaptcha.render){
+                    console.log('render success');
+                    console.log(this)
+                }
+                let widgetId=grecaptcha.render(id, {
+                    'sitekey': '6Le62qUUAAAAAN9EITa_yLNUKThYL0X7sBjZ_hBo',
+                    "theme":'light',
+                    "size":'normal',
+                    'callback': function (data) {//验证成功回调函数
+                        if(data.length!==0){
+                            successCallback(data,widgetId);
+                            setTimeout(() => {
+                                grecaptcha.reset(widgetId)
+                            }, 1500);
+                        }
+                    },
+                    "expired-callback":function(error){//验证失效回调函数
+                        expiredCallback(error)
+                        console.log('expired-callback')
+                    },
+                    "error-callback":function(error){//因为网络等问题无法验证，通过回调函数提醒用户重试
+                        errorCallback(error)
+                        console.log('error-callback')
+                    },
+
+                    });
+                    return widgetId;
+}
 export const geeTest = (id,interactive,successFun,closeFunc)=>{
     Sense.onClose(function(){
         closeFunc()
